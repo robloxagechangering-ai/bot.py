b49374041ea32e74a426af41b677f1f36075ef1e
robloxagechangering-ai <robloxagechangering@gmail.com>
2026-07-29 12:48:19 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from aiohttp import web
 # ==================================================
 # НАСТРОЙКИ
 # ==================================================
-BOT_TOKEN = "8816790244:AAHI3iLo448aPivteWHHrnk09gqE56mubhA"
+BOT_TOKEN = "8816790244:AAGQq9weI9GuUIAIINdXI9CeUtsbfpEANQc"
 ADMIN_IDS = [8625870625]
 PORT = int(os.environ.get("PORT", 8080))
 
